7c76aaaf7ed8f0da3312c51ce0e3c225d4ba29e5
rezagunawan <rezagunawan999@gmail.com>
2017-10-13 12:20:03 +0700

    Tugas Praktikum 1 Praktikum Mobile Programming Biodata

---
+++ app/src/main/java/com/example/mine/pertemuan1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mine.pertemuan1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//      Linear Layout
+//        setContentView(R.layout.activity_main);
+//      Relative Layout
+//        setContentView(R.layout.relative_layout);
+//       Scroll View
+        setContentView(R.layout.scroll_view);
+    }
+}
